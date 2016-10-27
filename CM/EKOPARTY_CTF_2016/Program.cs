6c262c2143775f44a582bebcbd70b638ca6ddbdf
enderdzz <417621265@qq.com>
2016-10-27 23:12:07 +0800

    Add the EKO CTF 2016's RE first 4 problems wp.
    First four problems' s wp all in the EKO_RE_WP.txt. And one attachment
    "Program.cs" belong to the problem No.2.

---
+++ CM/EKOPARTY_CTF_2016/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Globalization;
+
+namespace FlagGen
+{
+    public static class Program
+    {
+        public static string get_flag(string str)
+        {
+            int[] array = StringInfo.ParseCombiningCharacters(str);
+
+            int num = array.Length;
+
+            string[] array2 = new string[num];
+            int num2 = 0;
+            int num3 = num - 1;
+            if (num3 >= num2)
+            {
+                do
+                {
+                    if (num2 == array.Length - 1)
+                    {
+                        array2[num2] = str.Substring(num2);
+                    }
+                    else
+                    {
+                        int num4 = array[num2];
+                        array2[num2] = str.Substring(num4, array[num2 + 1] - num4);
+                    }
+                        num2++;
+                }
+                while (num2 != num3 + 1);
+            }
+            string[] array3 = array2;
+            Array.Reverse(array3);
+            return string.Join("", array3);
+        }
+        public static int Main(string[] argv)
+        {
+            string text = Program.get_flag("t#hs_siht_kc#f");
+            Console.WriteLine(text);
+            return 0;
+        }
+    }
+}
